e77bb91f7f033877d705d930551fdc7991dcebdb
Angel <EMAIL>
2023-09-29 22:18:06 -0400

    comments rendering

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API = process.env.REACT_APP_API_URL;
 
-export default function Comment() {
+export default function SinglePost() {
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
     const [isExpanded, setIsExpanded] = useState(false);
@@ -20,7 +20,7 @@ export default function Comment() {
         axios.post(`${API}/comments`, { content: newComment })
             .then((response) => {
                 setComments([...comments, response.data]); 
-                setNewComment(''); 
+                setNewComment('');
             })
             .catch((error) => {
                 console.error('Error submitting comment:', error);
@@ -70,57 +70,3 @@ export default function Comment() {
         </div>
     )
 }
-  
-
-
-// import { useState, useEffect } from "react";
-// import CommentForm from "./CommentForm";
-
-// const API = process.env.REACT_APP_API_URL;
-
-// function Comment({ comment, handleSubmit }) {
-//   const [viewEditForm, toggleEditForm] = useState(false);
-//   const [username, setUsername] = useState("");
-
-//   useEffect(() => {
-//     const fetchUsername = async () => {
-//       try {
-//         const response = await fetch(`${API}/profiles/${comment.user_id}`);
-//         const data = await response.json();
-//         setUsername(data.username);
-//       } catch (error) {
-//         console.error("Error fetching username:", error);
-//       }
-//     };
-
-//     fetchUsername();
-//   }, [comment.user_id]);
-
-//   const toggleView = () => {
-//     toggleEditForm(!viewEditForm);
-//   };
-
-//   return (
-//     <div className="Comment">
-//       <br />
-//       <br />
-//       <button onClick={toggleView}>Edit this comment...</button>
-//       <br />
-//       <br />
-//       {viewEditForm ? (
-//         <CommentForm
-//           commentDetails={comment}
-//           toggleView={toggleView}
-//           handleSubmit={handleSubmit}
-//         />
-//       ) : (
-//         <div>
-//           <p>{username}</p>
-//           <p>{comment.content}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Comment;
