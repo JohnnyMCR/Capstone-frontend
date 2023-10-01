ea030933e8be739d222917ef3abe647a40f62397
Angel <EMAIL>
2023-10-01 12:15:24 -0400

    got rid of useless code

--- src/Components/ForumCard.js
+++ src/Components/ForumCard.js
@@ -1,32 +1,6 @@
-import React, { useState, useEffect } from "react";
 import Comments from './Comment';
-import axios from "axios";
-
-const API = process.env.REACT_APP_API_URL;
 
 export default function ForumCard({ forum ,user }) {
-    const [userName, setUserName] = useState(null);
-
-    useEffect(() => {
-      if (forum.user_id) {
-        axios
-          .get(`${API}/profiles/${forum.user_id}`)
-          .then((response) => {
-            console.log('API Response:', response.data);
-            if (response.data && response.data.username) {
-              setUserName(response.data.username);
-            } else {
-              setUserName('Username not found');
-            }
-          })
-          .catch((error) => {
-            console.error('Error fetching username:', error);
-            setUserName('Error fetching username');
-          });
-      } else {
-        setUserName('User ID not defined');
-      }
-    }, [forum.user_id]); 
 
     return (
         <div className="column">
@@ -36,7 +10,7 @@ export default function ForumCard({ forum ,user }) {
                         <div className='column has-text-left is-full has-background-info'>
                             <h1 className='column is-one-half has-text-centered title has-text-primary'>{forum.title}</h1>
                             <span className='column is-one-quarter is-size-6 has-text-dark'>{forum.date}</span>
-                            <p className='column is-one-quarter is-size-6 has-text-dark'>{userName}</p>
+                            <p className='column is-one-quarter is-size-6 has-text-dark'>{forum.username}</p>
                             <p className='column is-one-quarter is-size-6 has-text-dark'>{forum.category}</p>
                             <p className='column is-one-quarter is-size-6 has-text-dark'>{forum.content}</p>
                             <Comments user={user}  />
