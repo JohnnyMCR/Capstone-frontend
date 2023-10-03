9fd403c5acf98c6af29e84dd9da739737f75a6f2
SamuelBAlba <brayanbalba24@gmail.com>
2023-10-02 23:50:15 -0400

    1st dashboard intro

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -10,7 +10,8 @@ export default function Comment({ user, forum_id }) {
     const [isExpanded, setIsExpanded] = useState(false);
     const [editingCommentId, setEditingCommentId] = useState(null);
 
-    console.log(comments)
+    // console.log(comments)
+    console.log(forum_id, 'testing forum ID')
 
     const toggleExpand = () => {
         setIsExpanded(!isExpanded);
@@ -83,7 +84,7 @@ export default function Comment({ user, forum_id }) {
             {isExpanded && (
                 <div className="expanded-content pl-5 py-2">
                     <div className="post-content column is-three-quarter is-size-6 has-background-light has-text-dark">
-                        content blah blah blah
+                        blaaa blaaa
                     </div>
                     <ul className='card has-background-info'>
                         {comments.map((comment, index) => (
