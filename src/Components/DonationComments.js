bb75c437e82e939e12e157788cbc0964b501cb26
MaxWattan <mcwattanac@gmail.com>
2023-10-08 13:39:29 -0400

    styled donation modal

--- src/Components/DonationComments.js
+++ src/Components/DonationComments.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import { AuthContext } from './AuthContext';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "./AuthContext";
 
 const API = process.env.REACT_APP_API_URL;
 
 export default function DonationComments({ donations_id }) {
   const { currentUser, auth } = useContext(AuthContext);
-  console.log(currentUser, auth, 'nav test');
+  console.log(currentUser, auth, "nav test");
   const [donationComments, setDonationComments] = useState([]);
-  const [newDonationComment, setNewDonationComment] = useState('');
+  const [newDonationComment, setNewDonationComment] = useState("");
   const [isExpanded, setIsExpanded] = useState(false);
 
-  console.log(donations_id, 'testing donation ID');
+  console.log(donations_id, "testing donation ID");
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
-  console.log(!isExpanded)
+  console.log(!isExpanded);
 
   const handleDonationCommentChange = (event) => {
     setNewDonationComment(event.target.value);
@@ -24,7 +24,7 @@ export default function DonationComments({ donations_id }) {
 
   const handleSubmitComment = async (e) => {
     e.preventDefault();
-  
+
     if (currentUser) {
       const actualNewDonationComment = {
         content: newDonationComment,
@@ -32,82 +32,86 @@ export default function DonationComments({ donations_id }) {
         donations_id,
         date: new Date().toLocaleDateString(),
       };
-  
+
       try {
-        const response = await axios.post(`${API}/donation-comments`, actualNewDonationComment);
-  
+        const response = await axios.post(
+          `${API}/donation-comments`,
+          actualNewDonationComment
+        );
+
         console.log(actualNewDonationComment);
         console.log(response.data);
-  
+
         const addedDonationComment = {
           ...response.data,
           username: currentUser.username,
         };
-  
+
         setDonationComments([...donationComments, addedDonationComment]);
-        setNewDonationComment('');
+        setNewDonationComment("");
       } catch (error) {
-        console.error('Error submitting comment:', error);
+        console.error("Error submitting comment:", error);
       }
     } else {
-      console.error('currentUser is not defined');
+      console.error("currentUser is not defined");
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${API}/donations/${donations_id}/donation-comments`);
+        const response = await axios.get(
+          `${API}/donations/${donations_id}/donation-comments`
+        );
         console.log(response.data);
         setDonationComments(response.data);
       } catch (error) {
-        console.warn('Error fetching comments:', error);
+        console.warn("Error fetching comments:", error);
       }
     };
-  
+
     fetchData();
   }, [donations_id]);
-  
+
   return (
-    <div className="column control is-flex is-justify-content-center is-align-items-center">
+    <div>
       <button
-        className="button is-medium is-rounded is-primary has-text-weight-bold"
+        className="button is-rounded is-primary has-text-weight-bold mt-3"
         onClick={toggleExpand}
       >
-        {isExpanded ? 'Hide Comments' : 'Show Comments'}
+        {isExpanded ? "Hide Comments" : "Show Comments"}
       </button>
+    <div className="column control is-flex is-justify-content-center is-align-items-center">
 
-<div>
-
-      {isExpanded && (
-        <div className="comments-box has-background-success px-3 py-3">
-          <h2 className="title is-4 has-text-primary">Comments</h2>
-          <ul>
-            {donationComments.map((comment) => (
-              <li key={comment.id}>
-                {comment.content} - Posted by {comment.username}
-              </li>
-            ))}
-          </ul>
-          <div className="comment-box">
-            <textarea
-              className="textarea"
-              placeholder="Add your comment..."
-              value={newDonationComment}
-              onChange={handleDonationCommentChange}
-            />
-            <button
-              className="button is-rounded has-text-weight-bold is-primary mt-3"
-              onClick={handleSubmitComment}
-            >
-              Submit Comment
-            </button>
+      <div>
+        {isExpanded && (
+          <div className="comments-box has-background-success px-3 py-3">
+            <h2 className="title is-4 has-text-primary">Comments</h2>
+            <ul className="has-text-dark py-5">
+              {donationComments.map((comment) => (
+                <li key={comment.id}>
+                  {comment.content} - Posted by {comment.username}
+                </li>
+              ))}
+            </ul>
+            <div className="comment-box">
+              <textarea
+                className="textarea"
+                placeholder="Add your comment..."
+                value={newDonationComment}
+                onChange={handleDonationCommentChange}
+              />
+              <button
+                className="button is-rounded has-text-weight-bold is-primary mt-3"
+                onClick={handleSubmitComment}
+              >
+                Submit Comment
+              </button>
+            </div>
           </div>
-          </div>
-      )}
-  </div>
+        )}
+      </div>
+    </div>
     </div>
   );
 }
-
-
