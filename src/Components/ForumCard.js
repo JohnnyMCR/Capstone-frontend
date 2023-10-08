d65304025f077db6fccb3e4431af81975feb3a3a
SamuelBAlba <brayanbalba24@gmail.com>
2023-10-08 14:53:59 -0400

    up to date

--- src/Components/ForumCard.js
+++ src/Components/ForumCard.js
@@ -1,24 +1,42 @@
-import Comments from './Comment';
+import Comments from './Comment'
+import React, { useState } from 'react';
 
-export default function ForumCard({ forum ,user }) {
-  console.log(forum)
+export default function ForumCard({ forum, user }) {
+  const [forumContent, setForumContent] = useState(forum.content.slice(0, 108));
 
-    return (
-        <div className="column">
-            <div className="card mb-5">
-                <div className="card-content">
-                    <div className='columns'>
-                        <div className='column has-text-left is-full has-background-info'>
-                            <h1 className='column is-one-half has-text-centered title has-text-primary'>{forum.title}</h1>
-                            <span className='column is-one-quarter is-size-6 has-text-dark'>{forum.date}</span>
-                            <p className='column is-one-quarter is-size-6 has-text-dark'>{forum.username}</p>
-                            <p className='column is-one-quarter is-size-6 has-text-dark'>{forum.category}</p>
-                            <p className='column is-one-quarter is-size-6 has-text-dark'>{forum.content}</p>
-                            <Comments user={user} forum_id={forum.id} forumContent={forum.content}  />
-                        </div>
-                    </div>
-                </div>
+  // Callback function to update forum content
+  const updateContent = (newContent) => {
+    setForumContent(forum.content);
+  };
+
+  return (
+    <div className="column">
+      <div className="card mb-5" style={{ boxShadow: '5px 5px 10px #32A6C3' }}>
+        <div className="card-content has-background-info">
+          <div className='columns'>
+            <div className='column has-text-left is-full'>
+              <h1 className='title has-text-dark' style={{ marginLeft: '10px' }}>{forum.title}</h1>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                  src="https://via.placeholder.com/150"
+                  alt="Generic Circular Placeholder"
+                  style={{
+                    width: '40px',
+                    height: '40px',
+                    borderRadius: '50%',
+                    objectFit: 'cover',
+                    backgroundColor: '#333'
+                  }}
+                />
+                <p className='is-size-6 has-text-dark ml-5'>{forum.username}</p>
+                <span className='is-size-6 has-text-dark ml-5'>{forum.date}</span>
+              </div>
+              <span className='column is full is-size 6 has-text-dark'>{forumContent}</span>
+              <Comments user={user} forum_id={forum.id} forum={forum} forumContent={forum.content} updateForumContent={updateContent} />
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
