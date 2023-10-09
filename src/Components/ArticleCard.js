b03154d803981622d6fc51e5e29219ac0c8934ad
MaxWattan <mcwattanac@gmail.com>
2023-10-08 22:28:06 -0400

    styled it better with title of related articles

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -85,11 +85,12 @@ export default function ArticleCard() {
         limitedArticles.map((article, index) => (
           <div className="card has-background-info px-3 py-3 mb-2" key={index}>
             <div className="columns">
-              <div className="column is-one-third">
-                <figure className="image is-128x128">
+              <div className="column is-one-third is-flex is-justify-content-center	is-align-content-center">
+                <figure className="image is-128x128 ">
                   <img
                     src={article.urlToImage || "https://bulma.io/images/placeholders/96x96.png"}
                     alt="Placeholder"
+                   
                   />
                 </figure>
               </div>
